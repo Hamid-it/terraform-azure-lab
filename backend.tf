01713f9152f0ae8ee24621727dbe18ccd85fcddb
Hamid-it <98198399+Hamid-it@users.noreply.github.com>
2026-07-01 13:52:26 +1000

    Remove storage account name from backend configuration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstate001"
     container_name       = "tfstate"
     key                  = "lab.tfstate"
   }
