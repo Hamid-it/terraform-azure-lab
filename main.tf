87895e1eb190305991babccc3773c5e6db0330ce
Hamid-it <98198399+Hamid-it@users.noreply.github.com>
2026-07-03 12:47:23 +1000

    Add Azure resources and random string generator

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
-resource "azurerm_resource_group" "lab" {
-  name     = "rg-terraform-lab"
-  location = "Australia East"
+data "azurerm_resource_group" "lab" {
+  name = "user-abticyikjhvj"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+resource "azurerm_storage_account" "lab" {
+  name                     = "stlab${random_string.suffix.result}"
+  resource_group_name      = data.azurerm_resource_group.lab.name
+  location                 = data.azurerm_resource_group.lab.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
